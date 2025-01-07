1174699d2321a3305c8cf30491ed8f404535f7d2
45111471 <mayra.soto@claro.com>
2025-01-07 14:26:06 -0500

    despliegeu en azure

--- proyecto_django/settings.py
+++ proyecto_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3wo00@h6^f=s_tbu1h-fjiy#-81=8ni)^^x+-+0ovbzevpi9q3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['meta07-a3adcadpbhe7gtd6.eastus-01.azurewebsites.net', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'proyecto_django/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
