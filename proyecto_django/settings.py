818488f159873bdc51497f9b03cfd597a1fd0cbd
45111471 <mayra.soto@claro.com>
2025-01-13 11:15:17 -0500

    actualizacion de settings

--- proyecto_django/settings.py
+++ proyecto_django/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'D:/desarrollo/METa/oficial/pruebas/solodjango/inicio/templates/inicio/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'inicio/templates/inicio/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
