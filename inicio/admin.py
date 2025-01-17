b34121f069b7c6f1d17a499f58787b473c0cafc5
45111471 <mayra.soto@claro.com>
2025-01-17 10:57:45 -0500

    actualización para el token

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Empresa
+from .models import Usuario  # Reemplaza con el nombre de tu modelo
+
+@admin.register(Empresa)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email', 'whatsapp_id', 'telefono', 'direccion', 'fecha_creacion')
+    search_fields = ('nombre', 'email', 'whatsapp_id')
+    
+
+
+admin.site.register(Usuario)
--- inicio/models.py
+++ inicio/models.py
@@ -3,8 +3,15 @@ from django.db import models
 
 class Empresa(models.Model):
     nombre = models.CharField(max_length=255)
-    identificador = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    whatsapp_id = models.CharField(max_length=255, unique=True)
+    access_token = models.TextField()
+    client_id = models.CharField(max_length=255, null=True, blank=True)  # Nuevo campo
+    client_secret = models.CharField(max_length=255, null=True, blank=True)  # Nuevo campo
     fecha_creacion = models.DateTimeField(auto_now_add=True)
+    telefono = models.CharField(max_length=15, null=True, blank=True)
+    direccion = models.TextField(null=True, blank=True)
+
     
     def __str__(self):
         return self.nombre
